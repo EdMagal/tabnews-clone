c3b486b2eba19776492effa500de587bec872d40
Eduardo Fernandes de Magalhaes Pimenta <ed83isme@gmail.com>
2024-04-08 19:03:24 +0000

    criação de páginas para teste, models e api. Feito um teste de integração com a API.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,6 @@
 import React from "react";
-import axios from "axios";
 
 function LandingPage() {
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("/api/motoristas");
-      console.log(response.data);
-    } catch (error) {
-      console.error("Erro ao obter dados:", error);
-    }
-  };
-
   return (
     <div className="landing-page">
       <h1>Página em construção</h1>
