1d5f886e5301f3adcf3408e6e11245fad0396dd7
Eduardo Fernandes de Magalhaes Pimenta <ed83isme@gmail.com>
2024-04-18 12:35:10 +0000

    finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseInfoResult = await database.query({
+    text: `SELECT 
+      current_setting('server_version') AS server_version,
+      current_setting('max_connections') AS max_connections,
+      (SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1) AS current_connections; 
+    `,
+    values: [databaseName],
+  });
+  const databaseVersionValue = databaseInfoResult.rows[0].server_version;
+  const maxConnections = parseInt(databaseInfoResult.rows[0].max_connections);
+  const openedConnections = databaseInfoResult.rows[0].current_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConnections,
+        current_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
